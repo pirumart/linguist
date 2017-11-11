c40d83a55bb2d9045ae0223bcb9fbc3af99f6bbd
pirupius <piruville@gmail.com>
2017-11-11 07:19:04 +0000

    fixes failing test

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 //***Other public routes should be added here
 
 Auth::routes();
